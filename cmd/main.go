982f637e70a0087a81cf1f228093212f84b7f114
bigPingRail <fmpanic@gmail.com>
2023-07-04 23:36:42 +0300

    router refactor

--- cmd/main.go
+++ cmd/main.go
@@ -11,8 +11,6 @@ import (
 	"sort"
 	"strings"
 	"time"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -45,53 +43,10 @@ func main() {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 
-	// Create the Gin router
-	r := gin.Default()
-	r.SetTrustedProxies(nil)
-	r.LoadHTMLGlob("templates/*.tmpl")
-
-	r.GET("/hc", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "ok",
-		})
-	})
-
-	r.GET("/", func(c *gin.Context) {
-		keys := make([]string, 0, len(result))
-		for key := range result {
-			keys = append(keys, key)
-		}
-
-		sort.Strings(keys)
-
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"Keys": keys,
-		})
-	})
-
-	r.GET("/values/:key", func(c *gin.Context) {
-		key := c.Param("key")
-		values, exists := result[key]
-		if !exists {
-			c.String(http.StatusNotFound, "Key not found")
-			return
-		}
-
-		c.HTML(http.StatusOK, "values.tmpl", gin.H{
-			"Title":  key,
-			"Values": values,
-		})
-	})
-
-	r.GET("/download/*link", func(c *gin.Context) {
-		link := c.Param("link")
-		utils.DownloadHandler(c.Writer, c.Request, link)
-	})
-
 	// Start the server
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%v", *utils.Port),
-		Handler: r,
+		Handler: router(result),
 	}
 
 	go func() {

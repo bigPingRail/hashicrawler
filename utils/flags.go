ffcd13f37ceeb2046bc1e4d87e455a46ef6ad348
bigPingRail <fmpanic@gmail.com>
2023-09-02 11:52:29 +0300

    add basic auth, auth flag

--- utils/flags.go
+++ utils/flags.go
@@ -3,6 +3,7 @@ package utils
 import "flag"
 
 var (
-	Port    = flag.Int("p", 8080, "listen port")
-	Caching = flag.Bool("c", false, "enable local caching")
+	Port    = flag.Int("p", 8080, "Listen port")
+	Caching = flag.Bool("c", false, "Enable local caching")
+	Auth    = flag.Bool("a", false, "Enable basic auth.\n By default login is 'admin' password 'testpass',\n you can override this values by setting up 'P_user' and 'P_password' environment variables")
 )
